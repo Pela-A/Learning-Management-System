0a36c791855cb4533fbad247f94a41bb6c915bb1
Tristan Knott <122324806+QuintsGuy@users.noreply.github.com>
2024-02-20 14:59:05 -0500

    Added Aside Images

--- include/aside.php
+++ include/aside.php
@@ -28,22 +28,58 @@
         <?php if($_SESSION['isSiteAdmin']): ?>
             <h4>Site Admin Controller</h4>
             <ul>
-                <li><a href="organizations.php?action=Viewer">Manage Organizations</a></li>
-                <li><a href="userAccount.php?action=Viewer">Manage User Accounts</a></li>
-                <li><a href="loginAttempts.php?action=Viewer">Manage User Login Attempts</a></li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\ModifyOrganization.png" alt="Modify Orgs">
+                    <a href="organizations.php?action=Viewer">Manage Organizations</a>
+                </li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\ValidateManageNewUsers.png" alt="Modify Users">
+                    <a href="userAccount.php?action=Viewer">Manage User Accounts</a>
+                </li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\LoginDashboard.png" alt="Login Dashboard">
+                    <a href="loginAttempts.php?action=Viewer">Login Dashboard</a>
+                </li>
             </ul>
         <?php endif; ?>
 
         <?php if($_SESSION['isOrgAdmin']): ?>
             <h4>Org Admin Controller</h4>
-            <ul><li><a href="orgControlPanel.php">Organization Control Panel</a></li></ul>
+            <ul>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\ModifyOrganization.png" alt="Modify Organizations">
+                    <a href="organizations.php?action=Viewer">Modify Organizations</a>
+                </li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\ModifyDepartments.png" alt="Modify Organizations">
+                    <a href="departments.php?action=Viewer">Modify Departments</a>
+                </li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\ModifyExistingUsers.png" alt="Modify Organizations">
+                    <a href="userAccount.php?action=Viewer">Modify User Accounts</a>
+                </li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\ValidateManageNewUsers.png" alt="Modify Organizations">
+                    <a href="userAccount.php?action=Validator">Validate New Users</a>
+                </li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\LoginDashboard.png" alt="Modify Organizations">
+                    <a href="loginAttempts.php?action=Viewer">Login Dashboard</a>
+                </li>
+            </ul>
         <?php endif; ?>
 
         <?php if($_SESSION['isTrainer']): ?>
             <h4>Training Manager</h4>
             <ul>
-                <li><a href="trainingEntry.php?action=Validator">User Training Validator</a></li>
-                <li><a href="trainingModules.php?action=Create">Create New Training Module</a></li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\ValidateCheckMark.png" alt="Validator">
+                    <a href="trainingEntry.php?action=Validator">Training Validator</a>
+                </li>
+                <li>
+                    <img src="..\assets\images\atlasPhotos\CreateTraining.png" alt="Create Training">
+                    <a href="trainingModules.php?action=Create">Create New Training Module</a>
+                </li>
                 <li><a href="trainingModules.php?action=ViewAll">Training Modules Viewer</a></li>
                 <li><a href="trainingEntry.php?action=ViewAll">Training Entry Viewer</a></li>
             </ul>
@@ -51,15 +87,34 @@
 
         <h4>Personal Training</h4>
         <ul>
-            <li><a href="trainingEntry.php?action=Create">Log Training Event</a></li>
-            <li><a href="trainingModules.php?action=ViewAll">Training Modules Viewer</a></li>
-            <li><a href="trainingEntry.php?action=ViewAll">View Past Training</a></li>
-            <li><a href="loginAttempts.php?action=Viewer">Login Attempts Manager</a></li>
+            <li>
+                <img src="..\assets\images\atlasPhotos\EnterTraining.png" alt="Enter Training">
+                <a href="trainingEntry.php?action=Create">Log Training Event</a>
+            </li>
+            <li>
+                <img src="..\assets\images\atlasPhotos\ViewTraining.png" alt="Training Viewer">
+                <a href="trainingModules.php?action=ViewAll">Training Modules Viewer</a>
+            </li>
+            <li>
+                <img src="..\assets\images\atlasPhotos\PastTraining.png" alt="Past Training">
+                <a href="trainingEntry.php?action=ViewAll">View Past Training</a>
+            </li>
+            <li>
+                <img src="..\assets\images\atlasPhotos\LoginDashboard.png" alt="Login Dashboard">
+                <a href="loginAttempts.php?action=Viewer">Login Dashboard</a>
+            </li>
         </ul>
 
 
-        <a href="userAccount.php?action=personalSettings">Account Settings</a>
-        <a href="logout.php">Logout</a>
+        <div>
+            <img src="..\assets\images\atlasPhotos\Settings.png" alt="Settings">
+            <a href="userAccount.php?action=personalSettings">Account Settings</a>
+        </div>
+
+        <div>
+            <img src="..\assets\images\atlasPhotos\LogoutIcon.png" alt="Logout">
+            <a href="logout.php">Logout</a>
+        </div>
 
     </aside>
 </body>
